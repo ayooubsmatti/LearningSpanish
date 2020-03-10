14158f54aee8892c3565864ff403f691e9d42a6d
ayooubsmatti <ayooubsmatti@gmail.com>
2020-03-10 23:57:45 +0100

    Use a ListView in NumbersActivity

--- app/src/main/java/com/example/learningspanish/NumbersActivity.java
+++ app/src/main/java/com/example/learningspanish/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.learningspanish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +14,42 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create a list of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+
+        // Find the root view so we can add child views to it
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        // Create a variable to keep track of the current index position
+        int index = 0;
+
+        // Keep looping until we've reached the end of the list (which means keep looping
+        // as long as the current index position is less than the length of the list)
+        while (index < words.size()) {
+            // Create a new TextView
+            TextView wordView = new TextView(this);
+
+            // Set the text to be word at the current index
+            wordView.setText(words.get(index));
+
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+
+            // Increment the index variable by 1
+            index++;
+        }
+
     }
 }
